b2d2443ac7fbb45e82cd5b3887645c2f868ea0bb
chengtao <2833909302@qq.com>
2026-05-19 11:01:12 +0800

    init

---
+++ src/main/java/org/ct/multiagentrecommendationsystem/MultiAgentRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package org.ct.multiagentrecommendationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAgentRecommendationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiAgentRecommendationSystemApplication.class, args);
+    }
+
+}
